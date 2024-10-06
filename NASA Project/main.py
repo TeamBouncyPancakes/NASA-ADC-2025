1923780de1fd00931410fa0bd6a218ea8acfaf5f
Teambouncypancakes <kidslearninglab099@gmail.com>
2024-10-05 18:14:35 -0700

    moon added

--- NASA Project/main.py
+++ NASA Project/main.py
@@ -8,36 +8,33 @@ app = Ursina()
 def input(key):
     if key == "escape" or key == "q":
         exit()
-    earth.x -= held_keys['a'] * time.dt * 10
-    earth.x += held_keys['d'] * time.dt * 10
-    earth.y += held_keys['w'] * time.dt * 10
-    earth.y -= held_keys['s'] * time.dt * 10
-    scale_increment = 0.5
-    if held_keys['k']:
-        earth.scale += (scale_increment, scale_increment, scale_increment)
-    if held_keys['m']:
-        earth.scale -= (scale_increment, scale_increment, scale_increment)
     earth.rotation_x += (held_keys['x'] * time.dt) * 1000
     earth.rotation_y += (held_keys['y'] * time.dt) * 1000
     camera.rotation_x += (held_keys['c'] * time.dt) * 100
     camera.rotation_y += (held_keys['v'] * time.dt) * 100
     camera.rotation_x -= (held_keys['b'] * time.dt) * 100
     camera.rotation_y -= (held_keys['n'] * time.dt) * 100
-    camera.position = (
-        camera.position[0] + (held_keys['g'] * time.dt * 10) - (held_keys['i'] * time.dt * 10),
-        camera.position[1] + (held_keys['h'] * time.dt * 10) - (held_keys['o'] * time.dt * 10),
-        camera.position[2] + (held_keys['j'] * time.dt * 10) - (held_keys['p'] * time.dt * 10)
-    )
+    move_speed = 10 * time.dt
+    if held_keys['g']: camera.position += (move_speed, 0, 0)
+    if held_keys['i']: camera.position -= (move_speed, 0, 0)
+    if held_keys['h']: camera.position += (0, move_speed, 0)
+    if held_keys['o']: camera.position -= (0, move_speed, 0)
+    if held_keys['j']: camera.position += (0, 0, -move_speed)
+    if held_keys['p']: camera.position -= (0, 0, -move_speed)
 
 
-#not working for some reason
 def update():
-    pass
+    earth.rotation_y -= 1
+    moon.rotation_y -= 0.0366
+
 class Planet:
-    def __init__(self, tripscale):
-        self.entity = Entity(model="sphere", texture="assets/textures-models/planet-textures/earth.jpg", scale=(tripscale, tripscale, tripscale))
-earth = Planet(2).entity
+    def __init__(self, tripscale, file, pos=(0, 0, 0)):
+        self.entity = Entity(model="sphere", texture=file, scale=(tripscale, tripscale, tripscale), position=pos)
+earth = Planet(2, "assets/textures-models/planet-textures/earth.jpg").entity
+moon = Planet(0.54, "assets/textures-models/planet-textures/moon.jpg", pos=(60, 0, 0)).entity
 earth.cull_faces, earth.double_sided = False, True
+moon.cull_faces, moon.double_sided = False, True
 EditorCamera()
+Sky(texture="assets/textures-models/space-textures/space4.jpg")
 mouse.locked = True
 app.run()
