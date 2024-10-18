d5898458746ab588127e48328728a7bde80cca6b
LeelaU815 <lgoelunni@gmail.com>
2024-10-17 21:02:13 -0400

    Update trajectory.py
    Color code drawing itself

--- NASA Project/trajectory.py
+++ NASA Project/trajectory.py
@@ -9,17 +9,24 @@ x_velocity = ms_data['Vx(km/s)[J2000-EARTH]'].to_numpy()
 y_velocity = ms_data['Vy(km/s)[J2000-EARTH]'].to_numpy()
 z_velocity = ms_data['Vz(km/s)[J2000-EARTH]'].to_numpy()
 
+times = ms_data['MISSION ELAPSED TIME (mins)'].to_numpy()
+
 test = np.array(x_velocity**2 + y_velocity**2 + z_velocity**2)
 
 overall_velocity = np.array(np.sqrt(test))
 
+colors = [color.red,color.white,color.green,color.orange,color.pink,color.yellow,color.blue]
+
+
+ci = 0
 
 def create_trajectory_line():
+    global ci
     scale_factor = 0.000125
     points = [Vec3(x,y,z) * scale_factor for x, y, z in zip(ms_data['Rx(km)[J2000-EARTH]'],ms_data['Ry(km)[J2000-EARTH]'],ms_data['Rz(km)[J2000-EARTH]'])]
-
-    line = Entity(model=Mesh(vertices=points, mode='line', thickness=2), color=color.red)
-    return points, line
+    current = [points[0],points[1]]
+    line = Entity(model=Mesh(vertices=current, mode='line', thickness=2), color=colors[ci])
+    return points, line, current
 
 app = Ursina(size=(2500,1000))
 
@@ -29,19 +36,25 @@ model = Entity(model='sphere', scale = (1,1,1),color=color.green)
 
 index = 1
 
-points, trajectory_line = create_trajectory_line()
+points, trajectory_line, current = create_trajectory_line()
 
 
 editor_camera = EditorCamera(pan_speed = 1000)
-
+ 
 camera.fov = 155
 
 point_index = 0
-overall_velocity = overall_velocity * 0.0005
+overall_velocity = overall_velocity * 0.001
 speed = overall_velocity[0]
 distance = 0
+phase = "Launch"
 
 distancetotal = Text(text='Distance Travelled (km): '+str(distance),x=0.55,y=0.47,size=0.04)
+phaselabel = Text(text='Phase: '+phase,x=-0.55,y=0.47,size=0.04)
+timelabel = Text(text="Time (min): "+str(times[0]),x=-0.55,y=0.4)
+xlabel = Text(text="X: "+str(points[0][0]),x=-0.75,y=0.47)
+ylabel = Text(text="Y: "+str(points[0][1]),x=-0.75,y=0.4)
+zlabel = Text(text="Z: "+str(points[0][2]),x=-0.75,y=0.34)
 
 def length(x,y,z,x2,y2,z2):
     x = x/0.000125
@@ -56,8 +69,21 @@ def length(x,y,z,x2,y2,z2):
     d = a**2 + b**2 + c**2
     return np.sqrt(d)
 
+def lerp(p1, p2, t):
+    x = (1-t) * p1
+    y = p2 * t 
+    return x + y
+def clean_line(current,next,f):
+    x = lerp(current.x,next.x,f)
+    y = lerp(current.y,next.y,f)
+    z = lerp(current.z,next.z,f)
+
+    return Vec3(x,y,z)
+
+inter = 0.75
+
 def update():
-    global point_index, speed, points, distance
+    global point_index, speed, points, distance, current, inter, colors,times,ci,phase
     # WASD camera movement
     if held_keys['w']: editor_camera.position += editor_camera.forward * time.dt * 5  # Move forward
     if held_keys['s']: editor_camera.position -= editor_camera.forward * time.dt * 5  # Move backward
@@ -67,18 +93,71 @@ def update():
     if held_keys['e']: editor_camera.position -= editor_camera.up * time.dt * 5       # Move down
 
     if point_index < len(points):
-        model.position = points[point_index]
+        currents = points[point_index]
+        if point_index + 1 == len(points):
+            next = points[0]
+        else:
+            next = points[point_index+1]
+
+        
+
+        if 8 <= times[point_index] < 48.23658:
+            ci = 0
+        elif 48.23658 <= float(times[point_index]) < 100.1082:
+            ci = 1
+            phase = "Initial firing to get into a higher Orbit"
+        elif 100.1082 <= float(times[point_index]) < 196.0945:
+            ci = 2
+            phase = "Orion separates from ICPS"
+        elif 196.0945 <= float(times[point_index]) < 283.6495:
+            ci = 3
+            phase = "Subsequent burning of fuel for higher orbit"
+        elif 283.6495 <= float(times[point_index]) < 792.4494:
+            ci = 4
+            phase = "Burn Fuel"
+        elif 792.4494 <= float(times[point_index]) < 1487.65:
+            ci = 5
+            phase = "Burn Fuel"
+        elif 1487.65 <= float(times[point_index]):
+            ci = 6
+            phase = "TransLunar Injection Burn Fuel"
+
+        model.position = clean_line(currents, next, inter)
         speed = overall_velocity[point_index]
-        x,y,z = points[point_index]
-        x2,y2,z2 = points[point_index+1]
+        x,y,z = model.position
+        x2,y2,z2 = next
+
+        current.append(points[point_index + 1])
+
         distance += length(x,y,z,x2,y2,z2)
         distance = np.round(distance, 2)
+
+        a = Mesh(vertices=current, mode='line', thickness=2)
+        trajectory_line.model = a
         distancetotal.text = 'Distance Travelled (km): '+str(distance)
-        point_index += int(speed * len(points))
+        timelabel.text = 'Time (min): '+str(float(times[point_index]))
+        phaselabel.text = 'Phase: '+phase
+        xlabel.text="X: "+str(round(points[point_index][0],5))
+        ylabel.text="Y: "+str(round(points[point_index][1],5))
+        zlabel.text="Z: "+str(round(points[point_index][2],5))
+
+        trajectory_line.color = colors[ci]
+        print(ci)
+
+        inter += speed * time.dt
+        point_index += int(speed*len(points)/5.301)
+
+        
+
+        if inter >= 1.0:
+            inter = 0
+            point_index += 1
     else:
         point_index = 0
         speed = overall_velocity[0]
         distance = 0
+        current = [points[0],points[1]]
+        
 
 
 app.run()
